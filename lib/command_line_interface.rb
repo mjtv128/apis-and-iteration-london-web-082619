bc0a8bc6fe187d4633c637398523c543a13ad2ca
jocular-source-7570 <mjtvan@gmail.com>
2019-09-13 10:49:57 +0000

    Automatically backed up by Learn

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,9 @@
 def welcome
-  # puts out a welcome message here!
+  "Welcome"
 end
 
 def get_character_from_user
   puts "please enter a character name"
+  input = gets.chomp.
   # use gets to capture the user's input. This method should return that input, downcased.
 end
